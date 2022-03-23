3c17e5d26b7c32d51361e8bba672626116ec6549
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-23 21:42:39 +0700

    Membuat Fungsi generateAccessToken

--- src/tokenize/TokenManager.js
+++ src/tokenize/TokenManager.js
@@ -2,7 +2,7 @@ const Jwt = require('@hapi/jwt');
 const InvariantError = require('../exceptions/InvariantError');
 
 const TokenManager = {
-
+  generateAccessToken: (payload) => Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY),
 };
 
 module.exports = TokenManager;

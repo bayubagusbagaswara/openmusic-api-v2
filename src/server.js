f2ae787cd8ef06e4d780e9086c35690a701bba88
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-23 17:13:27 +0700

    menambahkan onPreResponse di server.js

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const SongsService = require('./services/postgres/SongsService');
 const AlbumsService = require('./services/postgres/AlbumsService');
 const SongsValidator = require('./validator/songs');
 const AlbumsValidator = require('./validator/albums');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const albumsService = new AlbumsService();
@@ -37,32 +38,21 @@ const init = async () => {
       },
     }]);
 
-  /*
-  Di sini kamu bisa mendeklarasikan atau membuat extensions function untuk life cycle server onPreResponse, di mana ia akan mengintervensi response sebelum dikirimkan ke client. Di sana kamu bisa menetapkan error handling bila response tersebut merupakan client error.
-  */
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
 
-  // server.ext('onPreResponse', (request, h) => {
-  //   // mendapatkan konteks response dari request
-  //   const { response } = request;
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
 
-  //   if (response instanceof ClientError) {
-  //     // membuat response baru dari response toolkit sesuai kebutuhan error handling
-  //     const newResponse = h.response({
-  //       status: 'fail',
-  //       message: response.message,
-
-  //     });
-  //     newResponse.code(response.statusCode);
-  //     return newResponse;
-  //   }
-
-  //   // jika bukan ClientError, lanjutkan dengan response sebelumnya (tanpa terintervensi)
-  //   return response.continue || response;
-  // });
-
-  /*
-  Dengan begitu, di handler, kamu bisa fokus terhadap logika dalam menangani request, tanpa adanya error handling.
-  */
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+    return response.continue || response;
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);

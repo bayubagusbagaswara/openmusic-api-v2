28e41021de66579464573c1751ef0c0586d40d66
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-23 17:46:42 +0700

    register handler error di server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
-const songs = require('./api/songs');
+// albums
 const albums = require('./api/albums');
-const SongsService = require('./services/postgres/SongsService');
 const AlbumsService = require('./services/postgres/AlbumsService');
-const SongsValidator = require('./validator/songs');
 const AlbumsValidator = require('./validator/albums');
-const ClientError = require('./exceptions/ClientError');
+
+// songs
+const songs = require('./api/songs');
+const SongsService = require('./services/postgres/SongsService');
+const SongsValidator = require('./validator/songs');
+
+// error
+const errors = require('./api/errors');
 
 const init = async () => {
   const albumsService = new AlbumsService();
@@ -24,6 +29,9 @@ const init = async () => {
 
   await server.register([
     {
+      plugin: errors,
+    },
+    {
       plugin: albums,
       options: {
         service: albumsService,
@@ -38,21 +46,21 @@ const init = async () => {
       },
     }]);
 
-  server.ext('onPreResponse', (request, h) => {
-    // mendapatkan konteks response dari request
-    const { response } = request;
+  // server.ext('onPreResponse', (request, h) => {
+  //   // mendapatkan konteks response dari request
+  //   const { response } = request;
 
-    if (response instanceof ClientError) {
-      const newResponse = h.response({
-        status: 'fail',
-        message: response.message,
+  //   if (response instanceof ClientError) {
+  //     const newResponse = h.response({
+  //       status: 'fail',
+  //       message: response.message,
 
-      });
-      newResponse.code(response.statusCode);
-      return newResponse;
-    }
-    return response.continue || response;
-  });
+  //     });
+  //     newResponse.code(response.statusCode);
+  //     return newResponse;
+  //   }
+  //   return response.continue || response;
+  // });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);

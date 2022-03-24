30b3621883ad7f52dcaf6aa3c96b5f646ba5b8b2
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-24 10:08:57 +0700

    menambahan fungsi mapping

--- src/utils/index.js
+++ src/utils/index.js
@@ -24,12 +24,44 @@ const mapAlbumDBToModel = ({ id, name, year }) => ({
   year,
 });
 
+const mapUserDBToModel = ({
+  id, username, password, fullname,
+}) => ({
+  id,
+  username,
+  password,
+  fullname,
+});
+
 const mapPlaylistDBToModel = ({ id, name, owner }) => ({
   id,
   name,
   owner,
 });
 
+const mapPlaylistSongsDBToModel = ({ id, playlist_id, song_id }) => ({
+  id,
+  playlistId: playlist_id,
+  songId: song_id,
+});
+
+const mapCollaborationsDBToModel = ({ id, playlist_id, user_id }) => ({
+  id,
+  playlistId: playlist_id,
+  userId: user_id,
+});
+
+const mapPlaylistSongActivitiesDBToModel = ({
+  id, playlist_id, song_id, user_id, action, time,
+}) => ({
+  id,
+  playlistId: playlist_id,
+  songId: song_id,
+  userId: user_id,
+  action,
+  time,
+});
+
 module.exports = {
-  mapAlbumDBToModel, mapSongDBToModel, mapPlaylistDBToModel,
+  mapAlbumDBToModel, mapSongDBToModel, mapPlaylistDBToModel, mapUserDBToModel, mapPlaylistSongsDBToModel, mapCollaborationsDBToModel, mapPlaylistSongActivitiesDBToModel,
 };

4e0415e6c7a1cbe524de8f449c02ff71f8ba7563
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-03-24 09:38:50 +0700

    menambahkan plugin playlist handler

---
+++ src/api/playlist/handler.js
@@ -0,0 +1,57 @@
+class PlaylistsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postPlaylistHandler = this.postPlaylistHandler.bind(this);
+    this.getPlaylistsHandler = this.getPlaylistsHandler.bind(this);
+    this.deletePlaylistByIdHandler = this.deletePlaylistByIdHandler.bind(this);
+  }
+
+  // handler membuat playlist
+  async postPlaylistHandler(request, h) {
+    this._validator.validatePlaylistPayload(request.payload);
+
+    const { name } = request.payload;
+    const { id: credentialId } = request.auth.credentials;
+    const playlistId = await this._service.addPlaylist(name, credentialId);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        playlistId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  // ambil playlist
+  async getPlaylistsHandler(request) {
+    const { id: credentialId } = request.auth.credentials;
+    const playlists = await this._service.getPlaylists(credentialId);
+
+    return {
+      status: 'success',
+      data: {
+        playlists,
+      },
+    };
+  }
+
+  // menghapus playlist berdasarkan id
+  async deletePlaylistByIdHandler(request) {
+    const { id } = request.params;
+    const { id: credentialId } = request.auth.credentials;
+
+    await this._service.verifyPlaylistOwner(id, credentialId);
+    await this._service.deletePlaylistById(id, credentialId);
+
+    return {
+      status: 'success',
+      message: 'Playlist berhasil dihapus',
+    };
+  }
+}
+
+module.exports = PlaylistsHandler;
